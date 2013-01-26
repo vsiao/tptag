173ffcbfc07aef72f4e87aa8729e788579641060
Vincent Siao <vincentsiao@gmail.com>
2013-01-25 22:18:40 -0500

    move stuff around

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = require('./lib/Game');
+var Consolidate = require('consolidate');
+var Express = require('express');
+var Game = require('./lib/Game');
+var Path = require('path');
+
+var app = Express();
+
+app.configure(function() {
+  app.use(Express.json());
+  app.use(Express.static(Path.join(__dirname, 'public')));
+  app.engine('html', Consolidate.handlebars);
+  app.set('views', Path.join(__dirname, 'views'));
+  app.set('view engine', 'html');
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+module.exports = function(description) {
+  var express_listen = app.listen;
+  app.listen = function(port, callback) {
+    var server = express_listen.call(app, port, callback);
+    new Game(server, description);
+    return server;
+  };
+  app.hello = function() { console.log('hello world!'); };
+  return app;
+}
