1e3ed262afbc7d083a2892c28ec4947de39641e2
Vincent Siao <vincentsiao@gmail.com>
2013-01-25 23:21:02 -0500

    bounce player > 2; more infra

--- example/app.js
+++ example/app.js
@@ -1,5 +1,13 @@
 var TPTag = require('../');
-var app = TPTag({});
+var app = TPTag();
+
+app.action(['go', 'move', 'walk', 'head'], function(req, game) {
+});
+app.action(['look', 'inspect', 'check'], function(req, game) {
+});
+app.action('say', function(req, game) {
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on port %d in %s mode", port, app.settings.env);
--- index.js
+++ index.js
@@ -4,26 +4,39 @@ var Game = require('./lib/Game');
 var Path = require('path');
 
 var app = Express();
+app.game = new Game();
 
-app.configure(function() {
-  app.use(Express.json());
-  app.use(Express.static(Path.join(__dirname, 'public')));
-  app.engine('html', Consolidate.handlebars);
-  app.set('views', Path.join(__dirname, 'views'));
-  app.set('view engine', 'html');
-});
-
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-module.exports = function(description) {
+function TPTag() {
+  app.configure(function() {
+    app.use(Express.json());
+    app.use(Express.static(Path.join(__dirname, 'public')));
+    app.engine('html', Consolidate.handlebars);
+    app.set('views', Path.join(__dirname, 'views'));
+    app.set('view engine', 'html');
+  });
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
   var express_listen = app.listen;
   app.listen = function(port, callback) {
     var server = express_listen.call(app, port, callback);
-    new Game(server, description);
+    app.game.init(server);
     return server;
   };
-  app.hello = function() { console.log('hello world!'); };
   return app;
 }
+
+app.states = function(states) {
+  this.game.states = states;
+};
+
+app.action = function(verbs, callback) {
+  if (Object.prototype.toString.call(verbs) !== '[object Array]') {
+    verbs = [verbs];
+  }
+  for (i = 0; i < verbs.length; ++i) {
+    this.game.actions[verbs[i]] = callback;
+  }
+};
+
+module.exports = TPTag;
